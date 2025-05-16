4eb6db584678d6c2c6ed1bdf181875f4c3d3b62d
Emerson-cd <emersondcasco@gmail.com>
2025-05-16 14:45:14 -0300

    Add the New SEARCH_ON Column to the Dataframe that feeds the Multiselect

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,24 +18,30 @@ st.write("The name on your Smoothie will be:", name_on_order)
 
 cnx = st.connection("snowflake")
 session = cnx.session()
+
+
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
-st.dataframe(data=my_dataframe, use_container_width=True)
-st.stop()
+#st.dataframe(data=my_dataframe, use_container_width=True)
+#st.stop()
+pd_df = my_dataframe.to_pandas()
+#st.dataframe(pd_df)
+#st.stop()
 
 ingredient_list = st.multiselect('Choose up  5  ingredients:', my_dataframe, max_selections=5)
 
 if ingredient_list:
 
-    #st.write(ingredient_list)
-    #st.text(ingredient_list)
-
     ingredients_string = ''
 
     for fruit_chosen in ingredient_list:
         ingredients_string += fruit_chosen + ' '
+
+        search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        #st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+
         st.subheader(fruit_chosen + 'nutrition information')
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
-        sf_df = st.dataframe(data = smoothiefroot_response.json(), use_container_width = True) 
+        fruityvice_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + search_on)
+        sf_df = st.dataframe(data = fruityvice_response.json(), use_container_width = True) 
      
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order) values ('""" + ingredients_string + """', '""" + name_on_order + """')"""
     #st.write(my_insert_stmt)
