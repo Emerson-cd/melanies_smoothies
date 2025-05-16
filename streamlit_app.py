10102a295fabbc72ccd9dc463947c0fe4f3ecc72
Emerson-cd <emersondcasco@gmail.com>
2025-05-16 15:01:04 -0300

    idk

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,7 +33,7 @@ if ingredients_list:
 
     ingredients_string = ''
 
-    for fruit_chosen in ingredient_list:
+    for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
 
         search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
